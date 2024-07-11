142f40b5f87c0cb9d6f3cfcae39c79b6a742d70c
luis.rachope <luis.rachope@deal.com.br>
2024-07-11 16:22:27 -0300

    Testando os enums

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -8,8 +8,15 @@ qualquer = 22;
 // Arrays
 const lista = [];
 lista.push(13, 22.5, 22, 89, 1.58);
+// Enum
+var TipoTransacao;
+(function (TipoTransacao) {
+    TipoTransacao["DEPOSITO"] = "Dep\u00F3sito";
+    TipoTransacao["TRANSFERENCIA"] = "Transfer\u00EAncia";
+    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
+})(TipoTransacao || (TipoTransacao = {}));
 const novaTransacao = {
-    tipoTransacao: "",
+    tipoTransacao: TipoTransacao.DEPOSITO,
     data: new Date(),
     valor: 0
 };
